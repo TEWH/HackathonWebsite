2c77d2bef600271acd84e552552f4963988d7a2a
Alina Schroeder <adschroeder@utexas.edu>
2016-08-18 22:31:44 -0500

    here

--- src/server.py
+++ src/server.py
@@ -3,6 +3,8 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
+
 @app.route("/home")
 def hello():
     return render_template("index.html")
@@ -10,6 +12,14 @@ def hello():
 @app.route("/loggedIn")
 def afterLogin():
     return render_template("afterlogin.html")
+
+@app.route("/team_page")
+def teampage():
+    return render_template("team_page.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
 
 
 if __name__ == "__main__":
